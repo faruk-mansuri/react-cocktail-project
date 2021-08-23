2a35b86066178f85959f2139285ad8b888352af3
faruk <mansurifaruq998>
2021-08-23 16:28:19 +0530

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, [])
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search yor favorite cocktail</label>
+          <input id='name' type="text" ref={searchValue} onChange={searchCocktail}/>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas consequatur, amet nisi veniam itaque unde, fugit quam tenetur id hic odio dolorum, et magni numquam! Voluptatem amet laudantium vel quos! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt eius ducimus obcaecati sequi suscipit nobis earum vero blanditiis alias cumque.</p>
+
+    </section>
+  )
+}
+
+export default About
